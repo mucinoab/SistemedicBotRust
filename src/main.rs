d2457caa549111ebf2b15f640ca699d7a9f8556e
Bruno Muciño <mucinoab@gmail.com>
2020-07-11 19:53:42 -0500

    Logger, enums, regex

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,34 @@
-use dotenv::dotenv;
-use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
-//use postgres::{Client, NoTls};
-use postgres_openssl::MakeTlsConnector;
-use regex::Regex;
+use futures::StreamExt;
+use regex::{Regex, RegexSet};
+use std::collections::HashMap;
+use std::convert::From;
 use std::env;
 use std::time::Instant;
 use telegram_bot::Api;
+use telegram_bot::*;
 use tokio_postgres::{Error, NoTls};
 
 #[macro_use]
+extern crate log;
+#[macro_use]
 extern crate lazy_static;
-
-#[derive(Debug)]
-struct Filas {
-    clave: String,
-    generacion: String,
-    nombre: String,
-    apellidos: String,
-}
-
-#[derive(Debug)]
-enum Comando {
-    ClaveAzul,
-    NombreAzul,
-    ApellidoAzul,
-    ClaveInterno,
-    NombreInterno,
-    Ayuda,
-    Start,
-}
-use futures::StreamExt;
-use telegram_bot::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
-    builder.set_verify(SslVerifyMode::NONE);
-    let connector = MakeTlsConnector::new(builder.build());
-    let (client, connection) = tokio_postgres::connect(
-        &env::var("DATABASE_URL").expect("database_url no encontrada"),
-        connector,
-    )
-    .await?;
+    //     let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
+    //     builder.set_verify(SslVerifyMode::NONE);
+    //     let connector = MakeTlsConnector::new(builder.build());
+    //     let (client, connection) = tokio_postgres::connect(
+    //         &env::var("DATABASE_URL").expect("Database_url no encontrada o mal configurada."),
+    //         connector,
+    //     ).await?;
 
-    //let (client, connection) = tokio_postgres::connect("host=localhost user=bruno", NoTls).await?;
+    pretty_env_logger::init();
+
+    let (client, connection) = tokio_postgres::connect("host=localhost user=bruno", NoTls).await?;
 
     tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
+        connection.await.expect("Conexión fallida.");
     });
 
     let mut numero_de_filas: i64 = client
@@ -94,7 +74,7 @@ async fn main() -> Result<(), Error> {
             &[],
         )
         .await?
-        .into_iter()
+        .iter()
     {
         let clave: String = row.get(0);
         let generacion = match row.get(1) {
@@ -111,61 +91,42 @@ async fn main() -> Result<(), Error> {
         });
     }
 
-    lazy_static! {
-        static ref PATRON_AZULES: Regex = Regex::new(r"[Aa]\d{3}\*?").unwrap();
-        static ref PATRON_INTERNOS: Regex = Regex::new(r"[cC]\S{3}").unwrap();
+    let mut map: HashMap<String, &Filas> = HashMap::with_capacity(azules.len() + internos.len());
+
+    for datos in azules.iter().chain(internos.iter()) {
+        map.insert(datos.clave.to_uppercase(), datos);
     }
 
-    let api = Api::new(&env::var("TOKEN").expect("token no encontrada"));
+    let re_azules: Regex = Regex::new(r"[Aa]\d{3}\*?").unwrap();
+    let re_internos: Regex = Regex::new(r"[cC]\S{3}").unwrap();
+
+    let api = Api::new(&env::var("TOKEN").expect("Token no encontrado"));
     let mut stream = api.stream();
+
     while let Some(update) = stream.next().await {
         let update = update.unwrap();
         if let UpdateKind::Message(message) = update.kind {
             if let MessageKind::Text { ref data, .. } = message.kind {
-                println!("<{}>: {}", &message.from.first_name, data);
-
                 let now = Instant::now();
 
-                let mut comando = Comando::ClaveAzul;
+                info!("{}: {}", &message.from.first_name, data);
 
-                if data.contains("/c") {
-                    comando = Comando::ClaveAzul;
-                } else if data.contains("/n") {
-                    comando = Comando::NombreAzul;
-                } else if data.contains("/a") {
-                    comando = Comando::ApellidoAzul;
-                } else if data.contains("/ic") {
-                    comando = Comando::ClaveInterno;
-                } else if data.contains("/in") {
-                    comando = Comando::NombreInterno;
-                } else if data.contains("/h") {
-                    comando = Comando::Ayuda;
-                } else if data.contains("/s") {
-                    comando = Comando::Start;
-                } else {
-                    api.send(message.text_reply(String::from(
-                        "No te entendí...\nIntenta de nuevo o usa \"/h\" para ayuda.",
-                    )))
-                    .await
-                    .unwrap();
-                }
+                let comando = Comando::from(data);
 
                 match comando {
                     Comando::ClaveAzul => {
-                        for cap in PATRON_AZULES.captures_iter(data) {
+                        for cap in re_azules.captures_iter(data) {
                             let c = cap[0].to_uppercase();
-                            for lineas in &azules {
-                                if c == lineas.clave {
-                                    api.send(message.chat.text(format!(
-                                        "{} es {} {}, generación {}.",
-                                        c,
-                                        lineas.nombre,
-                                        lineas.apellidos.split(' ').next().unwrap(),
-                                        lineas.generacion,
-                                    )))
-                                    .await
-                                    .unwrap();
-                                }
+                            if let Some(lineas) = map.get(&c) {
+                                api.send(message.chat.text(format!(
+                                    "{} es {} {}, generación {}.",
+                                    c,
+                                    lineas.nombre,
+                                    lineas.apellidos.split(' ').next().unwrap(),
+                                    lineas.generacion,
+                                )))
+                                .await
+                                .unwrap();
                             }
                         }
                     }
@@ -175,20 +136,18 @@ async fn main() -> Result<(), Error> {
                     Comando::ApellidoAzul => {}
 
                     Comando::ClaveInterno => {
-                        for cap in PATRON_INTERNOS.captures_iter(data) {
+                        for cap in re_internos.captures_iter(data) {
                             let c = cap[0].to_uppercase();
-                            for lineas in &internos {
-                                if c == lineas.clave.to_uppercase() {
-                                    api.send(message.text_reply(format!(
-                                        "{} es {} {}, generación {}.",
-                                        c,
-                                        lineas.nombre,
-                                        lineas.apellidos.split(' ').next().unwrap(),
-                                        lineas.generacion,
-                                    )))
-                                    .await
-                                    .unwrap();
-                                }
+                            if let Some(lineas) = map.get(&c) {
+                                api.send(message.text_reply(format!(
+                                    "{} es {} {}, generación {}.",
+                                    c,
+                                    lineas.nombre,
+                                    lineas.apellidos.split(' ').next().unwrap(),
+                                    lineas.generacion,
+                                )))
+                                .await
+                                .unwrap();
                             }
                         }
                     }
@@ -196,45 +155,101 @@ async fn main() -> Result<(), Error> {
                     Comando::NombreInterno => {}
 
                     Comando::Ayuda => {
-                        api.send(message.text_reply(String::from(
-                            "Para buscar por clave usa \"/clave\" ó \"/c\" más las claves a buscar.\
-                            \nPara buscar por nombre usa \"/nombre\" ó \"/n\" más los nombres a buscar.\
-                            \nPara buscar por apellido usa \"/apellido\"  ó \"/a\"más los apellidos a buscar.\
-                            \nPara buscar internos por clave usa \"/iclave\" ó \"/ic\" más las claves.\
-                            \nPara buscar internos por nombre usa \"/inombre\" ó \"/in\" más los nombres a buscar.\
-                            \n\nEjemplo\
-                            \n\"/clave A101 A027 A007 A010*\"\
-                            \n\"/c a342\"\
-                            \n\"/iclave cKGr\"\
-                            \n\"/in Sam\"\
-                            \n\"/nombre Luis\"\
-                            \n\"/apellido Castillo\"\
-                            \n\nComparte https://t.me/sistemedicbot\
-                            \nCódigo Fuente https://github.com/mucinoab/SistemedicBot/"
-                        ))).await.unwrap();
+                        api.send(message.chat.text(AYUDA)).await.unwrap();
                     }
 
                     Comando::Start => {
-                        api.send(message.text_reply(String::from(
-                            "Hola soy el SistemedicBot.\
-                            \nPara buscar por clave usa \"/clave\" más las claves a buscar.\
-                            \nPara buscar por nombre usa \"/nombre\" más los nombres a buscar.\
-                            \nPara buscar por apellido usa \"/apellido\" más los apellidos a buscar.\
-                            \nPara buscar interno por clave usa \"/iclave\" más las claves.\
-                            \nPara buscar internos por nombre usa \"/inombre\" más los nombres a buscar.\
-                            \n\nEjemplo\
-                            \n\"/clave A101 A027 A007 A001 A010* A010\"\
-                            \n\"/iclave cKGr\"\
-                            \n\"/inombre Karol\"\
-                            \n\"/nombre Luis\"\
-                            \n\"/apellido Castillo\"\
-                            \n\nPara ayuda usa /help"
-                        ))).await.unwrap();
+                        api.send(message.chat.text(START)).await.unwrap();
+                    }
+
+                    Comando::None => {
+                        api.send(message.chat.text(NO_COMANDO)).await.unwrap();
                     }
                 };
-                println!("{:#?}", Instant::now().duration_since(now));
+                info!("{:#?}", Instant::now().duration_since(now));
             }
         }
     }
     Ok(())
 }
+
+#[derive(Debug)]
+struct Filas {
+    clave: String,
+    generacion: String,
+    nombre: String,
+    apellidos: String,
+}
+
+#[derive(Debug)]
+enum Comando {
+    ClaveAzul,
+    NombreAzul,
+    ApellidoAzul,
+    ClaveInterno,
+    NombreInterno,
+    Ayuda,
+    Start,
+    None,
+}
+
+impl From<&String> for Comando {
+    fn from(item: &String) -> Self {
+        lazy_static! {
+            static ref RE: RegexSet =
+                RegexSet::new(&[r"\/c", r"\/in", r"\/n", r"\/a", r"\/ic", r"\/h", r"\/s",])
+                    .unwrap();
+        }
+
+        let matches = RE.matches(item);
+
+        if matches.matched(0) {
+            Self::ClaveAzul
+        } else if matches.matched(1) {
+            Self::NombreInterno
+        } else if matches.matched(2) {
+            Self::NombreAzul
+        } else if matches.matched(3) {
+            Self::ApellidoAzul
+        } else if matches.matched(4) {
+            Self::ClaveInterno
+        } else if matches.matched(5) {
+            Self::Ayuda
+        } else if matches.matched(6) {
+            Self::Start
+        } else {
+            Self::None
+        }
+    }
+}
+
+static START: &str = "Hola soy el SistemedicBot.\
+                    \nPara buscar por clave usa \"/clave\" más las claves a buscar.\
+                    \nPara buscar por nombre usa \"/nombre\" más los nombres a buscar.\
+                    \nPara buscar por apellido usa \"/apellido\" más los apellidos a buscar.\
+                    \nPara buscar interno por clave usa \"/iclave\" más las claves.\
+                    \nPara buscar internos por nombre usa \"/inombre\" más los nombres a buscar.\
+                    \n\nEjemplo\
+                    \n\"/clave A101 A027 A007 A001 A010* A010\"\
+                    \n\"/iclave cKGr\"\
+                    \n\"/inombre Karol\"\
+                    \n\"/nombre Luis\"\
+                    \n\"/apellido Castillo\"\
+                    \n\nPara ayuda usa /help";
+
+static AYUDA: &str = "Para buscar por clave usa \"/clave\" ó \"/c\" más las claves a buscar.\
+                    \nPara buscar por nombre usa \"/nombre\" ó \"/n\" más los nombres a buscar.\
+                    \nPara buscar por apellido usa \"/apellido\"  ó \"/a\"más los apellidos a buscar.\
+                    \nPara buscar internos por clave usa \"/iclave\" ó \"/ic\" más las claves.\
+                    \nPara buscar internos por nombre usa \"/inombre\" ó \"/in\" más los nombres a buscar.\
+                    \n\nEjemplo\
+                    \n\"/clave A101 A027 A007 A010*\"\
+                    \n\"/c a342\"\
+                    \n\"/iclave cKGr\"\
+                    \n\"/in Sam\"\
+                    \n\"/nombre Luis\"\
+                    \n\"/apellido Castillo\"\
+                    \n\nComparte https://t.me/sistemedicbot\
+                    \nCódigo Fuente https://github.com/mucinoab/SistemedicBot/";
+
+static NO_COMANDO: &str = "No te entendí...\nIntenta de nuevo o usa \"/h\" para ayuda.";
